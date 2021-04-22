c67a8032bd3ba2910755c5dae67e1f4b9760b378
Dussan Freire <73646620+DussanFreire@users.noreply.github.com>
2021-04-22 10:20:27 -0400

    refactorizations

--- RealEstateSelling/Program.cs
+++ RealEstateSelling/Program.cs
@@ -6,13 +6,11 @@ namespace RealEstateSelling
     {
         static void Main(string[] args)
         {
-            // Create price watch for Carrots and attach restaurants that buy carrots from suppliers.
             House house = new House(123, 70100);
             house.Attach(new PotentialBuyer("John Silez", 60000));
             house.Attach(new PotentialBuyer("Marco Torrico", 62000));
             house.Attach(new PotentialBuyer("Raul Mendoza", 64444));
 
-            // Fluctuating carrot prices will notify subscribing restaurants.
             house.Price = 75000;
             house.Price = 63000;
             house.Price = 61000;
